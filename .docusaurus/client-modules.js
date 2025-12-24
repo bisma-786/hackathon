ef64bd02bc19975eed8f771be318ceb14f1e0d0c
Bisma Gondal <bismagondal786@gmail.com>
2025-12-24 18:21:54 +0500

    initial commit

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,7 +1,7 @@
 export default [
-  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book\\book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book\\book\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book\\book\\src\\css\\custom.css"),
+  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book - Copy\\book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book - Copy\\book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book - Copy\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book - Copy\\book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\Bisma Gondal\\Desktop\\ai-driven-book - Copy\\book\\src\\css\\custom.css"),
 ];
